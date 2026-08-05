d61a694db2dba2f24870ccc69d480daa3c119c64
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-08-05 06:28:51 +0300

    create splash view

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -5,6 +5,6 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Container());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
-  runApp(const BooklyApp());
+  runApp(const Bookly());
 }
 
-class BooklyApp extends StatelessWidget {
-  const BooklyApp({super.key});
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
   }
 }
